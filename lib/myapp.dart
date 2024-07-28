48a6e5dc93ad7af64be95b1f8e2e8353969a7cda
Abdulrahmn <syamabdo382@gmail.com>
2024-07-28 14:59:26 +0300

    build note item

--- lib/myapp.dart
+++ lib/myapp.dart
@@ -10,8 +10,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(), // dark mode or light mode
-      //theme: ThemeData(brightness: Brightness.dark, scaffoldBackgroundColor: ), // if i want to change the degree of dark of any part of screen
       home:const note_app_view(),
     );
   }
---
+++ lib/widgets/node_item.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NodeItem extends StatelessWidget {
+  const NodeItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(0xffFFCC80),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 20,bottom: 30,right: 10,left: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: Padding(
+                padding: const EdgeInsets.only(bottom: 16),
+                child: Text("Flutter tips",style: TextStyle(color: Colors.black,fontSize: 30),),
+              ),
+              subtitle: Text("Abdo Ahmed",style: TextStyle(color: Colors.black.withOpacity(.4),fontSize: 20)),
+              trailing: IconButton(onPressed: (){},icon: Icon(Icons.delete,size: 40,),color: Colors.black,),
+
+            ),
+            SizedBox(height: 50,),
+            Padding(
+              padding: const EdgeInsets.only(right: 35),
+              child: Text("May21 , 2022",style: TextStyle(color: Colors.black.withOpacity(.4),fontSize: 18),),
+            )
+          ]
+        ),
+      ),
+    );
+  }
+}
