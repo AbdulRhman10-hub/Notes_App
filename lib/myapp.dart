5083a014b7499f4e1d2b67ad453d6297d6fd33eb
Abdulrahmn <syamabdo382@gmail.com>
2024-09-24 22:26:18 +0300

    Provide add note cubit

--- lib/myapp.dart
+++ lib/myapp.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note_app/Cubits/add_note_cubit/add_note_cubit.dart';
 
 import 'features/ui/note_app_view.dart';
 class MyApp extends StatelessWidget {
@@ -7,10 +9,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      theme: ThemeData.dark(),
-      debugShowCheckedModeBanner: false,
-      home:const note_app_view(),
+    return  MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => AddNoteCubit()),
+      ],
+      child: MaterialApp(
+        theme: ThemeData.dark(),
+        debugShowCheckedModeBanner: false,
+        home:const note_app_view(),
+      ),
     );
   }
 }
