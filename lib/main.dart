8aa28bb3be620ccb9ae67b7b8ce6f5b8c7f4e967
Abdulrahmn <syamabdo382@gmail.com>
2024-09-24 21:32:07 +0300

    Register Adapter

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:note_app/constants/constants.dart';
 
+import 'models/note_model.dart';
 import 'myapp.dart';
 
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
+  Hive.registerAdapter(NoteModel as TypeAdapter); // register the type adaptor for the type adapter to define it to hive
   runApp(const MyApp());
 }
 
